bf9827ecdf896554b69d2eb0470a8b5db6d06937
jacktufts10 <jtufts@risd.edu>
2021-02-08 13:24:25 -0600

    update02-08

--- js/script.js
+++ js/script.js
@@ -1,21 +1,2 @@
 
-  
-    
-    
-const tl = new TimelineMax();
-
-const rule = CSSRulePlugin.getRule(".img-container:after");
-
-tl.to("main-image", { duration: 0.5, css: { visibility: "visible" } })
-  .to(rule, { duration: 1, width: "100%", ease: "Power2.ease" })
-  .to(rule, { duration: 0, right: 0, left: "unset" })
-  .to(rule, { duration: 1, width: "0%", ease: "Power2.ease" })
-  .to("img", { duration: 0.2, opacity: 1, delay: -1 })
-  .from("img", {
-    duration: 1,
-    scale: 1.4,
-    ease: "Power2.easeInOut",
-    delay: -1.2
-  });
-
 
